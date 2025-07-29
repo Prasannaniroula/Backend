28823e9bc6bb5d633d914a2fd5591f8efb61fa18
Prasannaniroula <Prasannaniroula5@gmail.com>
2025-07-29 21:06:20 +0545

    setup router and controller and an introduction of postman post

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import connectDB from "./db/index.js";
-import express from "express";
-const app = express();
+import {app} from './app.js';
+import dotenv from 'dotenv';
+dotenv.config({
+  path:'./env',
+});
 
 connectDB()
   .then(() => {
